fa2bd7a9bb1470189087a73a75810fc32eab1db2
Talles Eduardo de Almeida Sousa <125715792+TallesEduardoAlmeidaSousa@users.noreply.github.com>
2023-05-12 23:57:41 -0300

    Projeto_Armários(com JavaScript)

---
+++ armarios_projetos/armarios.js
@@ -0,0 +1,40 @@
+function mudarStatus(botao) {
+  
+    if (botao.innerHTML === "OCUPADO") {
+        botao.style.backgroundColor = "rgb(228, 228, 0)";
+        botao.innerHTML = "EM MANUTENÇÃO";
+      } else if (botao.innerHTML === "EM MANUTENÇÃO") {
+        botao.style.backgroundColor = "rgb(0, 255, 64)";
+        botao.innerHTML = "DESOCUPADO";
+      } else {
+        botao.style.backgroundColor = "rgb(221, 0, 0)";
+        botao.innerHTML = "OCUPADO";
+      }
+    
+    }
+    
+    if (botao.innerHTML === "EM MANUTENÇÃO") {
+        botao.style.backgroundColor = "yellow";
+        botao.innerHTML = "EM MANUTENÇÃO";
+      } else if (botao.innerHTML === "EM MANUTENÇÃO") {
+     
+        botao.style.backgroundColor = "green";
+        botao.innerHTML = "DESOCUPADO";
+      } else {
+        botao.style.backgroundColor = "red";
+        botao.innerHTML = "OCUPADO";
+      }
+     
+
+    if (botao.innerHTML === "DESOCUPADO") {
+        botao.style.backgroundColor = "yellow";
+        botao.innerHTML = "EM MANUTENÇÃO";
+      } else if (botao.innerHTML === "EM MANUTENÇÃO") {
+        botao.style.backgroundColor = "green";
+        botao.innerHTML = "DESOCUPADO";
+      } else {
+        botao.style.backgroundColor = "red";
+        botao.innerHTML = "OCUPADO";
+      }
+    
+    
